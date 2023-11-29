0dedba15f28953204167357dc01fa88fb960d44c
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-11-29 00:12:22 +0000

    Edit home html

--- things/views.py
+++ things/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    form = ThingForm()
+     if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  
+    else:
+        form = ThingForm() 
     return render(request, 'home.html', {'form': form})
