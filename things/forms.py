ab9ae68da279711653b12d5194dc6ce2d78eeb50
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-11-29 00:09:50 +0000

    Add thing form and modify views

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea(attrs={'cols': 40, 'rows': 5}), 'quantity': forms.NumberInput()}
 # Create your forms here.
